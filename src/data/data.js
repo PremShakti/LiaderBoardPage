9b7910ccbff1b09c34359f255b51980fbc9caaed
PremShakti <premshakti.ps@gmail.com>
2024-05-28 17:37:47 +0530

    firstpush

---
+++ src/data/data.js
@@ -0,0 +1,23 @@
+import { SiWindows } from "react-icons/si";
+import { FaCaretDown } from "react-icons/fa";
+export const navbar=[
+    {title:'LeaderBoard', leftIcon:<SiWindows/>,rightIcon:''},
+    {title:'Historical Trading', leftIcon:'',rightIcon:<FaCaretDown size={12}/>},
+    {title:'Historical Chart', leftIcon:'',rightIcon:''},
+    {title:'Scanners', leftIcon:'',rightIcon:''},
+    {title:'Alerts', leftIcon:'',rightIcon:''},
+    {title:'Basic Backtest', leftIcon:'',rightIcon:''},
+    {title:'Advanced Backtest', leftIcon:'',rightIcon:''},
+    {title:'Pricing', leftIcon:'',rightIcon:''},
+    {title:'My Earnings', leftIcon:'',rightIcon:''},
+]
+
+// LeaderBoard
+// Historical Trading
+// Historical Chart
+// Scanners
+// Alerts
+// Basic Backtest
+// Advanced Backtest
+// Pricing
+// My Earnings
